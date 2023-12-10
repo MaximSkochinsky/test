ab75e16677c469e026a147df04c120022d4dd8ee
MaximSkochinsky <m.skochinsky@mail.ru>
2023-12-10 20:28:59 +0300

    deploy version fresh dependencies

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,23 +1,29 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './rootReducer';
+import { createStore, applyMiddleware, compose } from "redux";
+import { thunk } from "redux-thunk";
+import rootReducer from "./rootReducer";
 
+const persistentState = localStorage.getItem("state")
+  ? JSON.parse(localStorage.getItem("state"))
+  : {};
 
-const persistentState = localStorage.getItem('state') ? JSON.parse(localStorage.getItem('state')) : {}
+const composeEnhancers =
+  (process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null) || compose;
 
-
-const composeEnhancers = (process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null) || compose;
-
-
-const store = () => createStore(rootReducer, persistentState, composeEnhancers(applyMiddleware(thunk)));
+const store = () =>
+  createStore(
+    rootReducer,
+    persistentState,
+    composeEnhancers(applyMiddleware(thunk))
+  );
 
 const Store = store();
 
-
 Store.subscribe(() => {
-    const state = Store.getState();
-    const serializedState = JSON.stringify(state);
-    localStorage.setItem('state', serializedState);
-  })
+  const state = Store.getState();
+  const serializedState = JSON.stringify(state);
+  localStorage.setItem("state", serializedState);
+});
 
 export default Store;
